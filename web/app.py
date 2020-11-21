73415eb7ecc8fc9942516bbccaeaaac4dac62feb
Lee Chun Hao <meowinhere@gmail.com>
2020-11-21 14:24:19 +0800

    Improved Algorithm Efficiency

--- web/app.py
+++ web/app.py
@@ -1,27 +1,59 @@
+import plotly
 import requests
 import pandas as pd
+import cufflinks as cf
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('Agg')
+# import matplotlib.pyplot as plt
 
 from FTX.client import Client
 
 from flask import Flask, request, abort, render_template
 
-from pandas.plotting import parallel_coordinates, table
+# from pandas.plotting import parallel_coordinates, table
+
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 
 # 全域變數
 app = Flask(__name__)
 client = Client('', '')
+cf.set_config_file(offline=True)
 
-# Pandas 設定
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('max_colwidth',100)
-pd.set_option('display.width', 5000)
-pd.set_option('display.unicode.ambiguous_as_wide', True)
-pd.set_option('display.unicode.east_asian_width', True)
+def get_k_line(symbol, resolution, start='', end='', limit=500):
+    payload = {
+        'resolution': resolution,
+        'limit': limit
+    }
+
+    if start:
+        payload.update({
+            'start_time': start
+        })
+
+    if end:
+        payload.update({
+            'end_time': end
+        })
+
+    data = requests.get(f'https://ftx.com/api/markets/{symbol}/candles', params=payload).json()['result']
+    date = [i['startTime'] for i in data]
+    line = [i['close'] for i in data]
+    df = pd.DataFrame(date, columns=['datetime'])
+    df = df.set_index('datetime')
+    df[symbol] = line
+
+    return df
+
+def get_funding_rate(symbol):
+    data = requests.get(f'https://ftx.com/api/funding_rates?future={symbol}').json()['result']
+    date = [i['time'] for i in data]
+    rate = [i['rate'] * 100 for i in data]
+    df = pd.DataFrame(date, columns=['datetime'])
+    df = df.set_index('datetime')
+    df['rate'] = rate
+
+    return df
 
 @app.route("/")
 def home():
@@ -32,9 +64,8 @@ def home():
 
     rateSum = []
     for coin in df.Name:
-        sum = 0
-        for data in client.get_public_single_funding_rates(coin):
-            sum += data['rate']
+        data = pd.DataFrame(client.get_public_single_funding_rates(coin))
+        sum = float(data.rate.sum())
         rateSum.append(round(abs(sum) * 100, 4))
     
     df['Rates (500 hrs)'] = rateSum
@@ -50,29 +81,26 @@ def perpetual(coin):
 
     currentPrice           = client.get_public_single_future(coin)['last']
     quarterPrice           = client.get_public_single_future(quarter)['last']
+
     premiumPrice           = round(quarterPrice - currentPrice, 2)
     premiumPriceRate       = round((premiumPrice / currentPrice) * 100, 2)
 
     nextFundingRate        = round(client.get_public_future_stats(coin)['nextFundingRate'] * 100, 4)
     lastFundingRate        = round(client.get_public_single_funding_rates(coin)[0]['rate'] * 100, 4)
 
-    currentKLine           = pd.DataFrame(client.get_public_k_line(coin, 3600, 500)).close
-    quarterKLine           = pd.DataFrame(client.get_public_k_line(quarter, 3600, 500)).close
+    currentKLine           = get_k_line(coin, 3600)
+    quarterKLine           = get_k_line(quarter, 3600)
 
-    fundingRateHistory     = pd.DataFrame(client.get_public_single_funding_rates(coin)).rate * 100
+    df = pd.concat([currentKLine, quarterKLine], axis=1)
+    df.index = pd.to_datetime(df.index)
+    fig = df.iplot(asFigure=True)
+    plotly.offline.plot(fig, filename=f"/app/templates/{coin}_premium.html")
 
-    plt.rcParams['figure.figsize'] = (8.0, 4.0)
-    currentKLine.plot(label = coin + ' Price')
-    quarterKLine.plot(label = quarter + ' Price')
-    plt.legend()
-    plt.savefig(f'/app/static/img/{coin}.png')
-    plt.cla()
 
-    plt.rcParams['figure.figsize'] = (8.0, 4.0)
-    fundingRateHistory.plot(label = coin + ' Rate')
-    plt.legend()
-    plt.savefig(f'/app/static/img/{coin}-Rate.png')
-    plt.cla()
+    rate = get_funding_rate(coin)
+    rate.index = pd.to_datetime(rate.index)
+    fig = rate.iplot(asFigure=True)
+    plotly.offline.plot(fig, filename=f"/app/templates/{coin}_rate.html")
 
     return render_template('coin.html', coin = coin, currentPrice = currentPrice, quarterPrice = quarterPrice, premiumPrice = premiumPrice, premiumPriceRate = premiumPriceRate, nextFundingRate = nextFundingRate, lastFundingRate = lastFundingRate)
 
--- web/test.py
+++ web/test.py
@@ -1,23 +1,9 @@
 import requests
 import pandas as pd
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-from FTX.client import Client
-
-from flask import Flask, request, abort, render_template
-
-from pandas.plotting import parallel_coordinates, table
-
-# Pandas 設定
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-pd.set_option('max_colwidth',100)
-pd.set_option('display.width', 5000)
-pd.set_option('display.unicode.ambiguous_as_wide', True)
-pd.set_option('display.unicode.east_asian_width', True)
-
-client = Client('', '')
-print (pd.DataFrame(client.get_public_all_perpetual_futures()).to_html())
+data = requests.get('https://ftx.com/api/funding_rates?future=PAXG-PERP').json()['result']
+df = pd.DataFrame(data)
+df = df.set_index('time')
+df = df.drop(columns=['future'])
+x = float(df.sum())
+print (x)
